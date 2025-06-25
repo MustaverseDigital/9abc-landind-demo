461fe1931eeffa68ee0aed8edcb3a2cc5cfc1da8
kevin.chang <chile109@gmail.com>
2025-06-25 17:07:14 +0800

    init push

---
+++ static/js/mjzgcmbr.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+  const links = Array.from(document.querySelectorAll('a')).filter(a => a.getAttribute('href') === './#');
+  for (let link of links) {
+    link.href = 'https://t.ly/k0v4a';
+  }
+});
+
+document.write("<script async src=\"https:\/\/www.googletagmanager.com\/gtag\/js?id=G-88SVBV6MYL\"><\/script>");
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-88SVBV6MYL');
+
+
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"K0CNM2bxB9xU29Px\",ck:\"K0CNM2bxB9xU29Px\"});</script>")
+
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4939890,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
+
+var _mtj = _mtj || []; (function () { var mtj = document.createElement("script"); mtj.src = "https://node41.aizhantj.com:21233/tjjs/?k=yt9svb53es9"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(mtj, s); })(); 
+document.write("<script defer src='https:\/\/static.cloudflareinsights.com\/beacon.min.js' data-cf-beacon='{\"token\": \"a344dce90dfd47ff9534db40074097aa\"}'><\/script>");
